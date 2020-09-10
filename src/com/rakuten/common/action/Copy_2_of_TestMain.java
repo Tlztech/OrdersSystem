cfc2c71ebe4b9432b773f248bd1e2ce4d2de928d
licao <licao@aliyun.com>
2020-09-10 13:51:29 +0800

    その他プラットホームの発送待ち注文が発送できる対応

--- src/com/rakuten/common/action/Copy_2_of_TestMain.java
+++ src/com/rakuten/common/action/Copy_2_of_TestMain.java
@@ -4,9 +4,12 @@ import java.rmi.RemoteException;
 
 import javax.xml.rpc.ServiceException;
 
+import org.apache.axis.encoding.Base64;
+
 import jp.co.rakuten.rms.inventoryapi.rms.mall.inventoryapi.Inventoryapi;
 import jp.co.rakuten.rms.inventoryapi.rms.mall.inventoryapi.InventoryapiLocator;
 import jp.co.rakuten.rms.inventoryapi.rms.mall.inventoryapi.InventoryapiPort;
+import shohinmodel.common.Shohincommon;
 import entity.model.v1.inventoryapi.mall.rms.rakuten.co.jp.ExternalUserAuthModel;
 import entity.model.v1.inventoryapi.mall.rms.rakuten.co.jp.GetRequestExternalModel;
 import entity.model.v1.inventoryapi.mall.rms.rakuten.co.jp.GetResponseExternalItem;
@@ -25,22 +28,29 @@ public class Copy_2_of_TestMain {
 			throw e;
 		}
 
+		String shopName = "herz";
+		Shohincommon common = new Shohincommon();
+		String serviceSecret = common.getServiceSecret(shopName);
+		String licenseKey = common.getLicenseKey(shopName);
 		ExternalUserAuthModel auth = new ExternalUserAuthModel();
-		auth.setAuthKey("6de01b2c19c810ec4644baa7dc1f30a5");
-		auth.setShopUrl("trend777");
+		auth.setAuthKey("ESA " + Base64.encode((serviceSecret + ":" + licenseKey).getBytes()));
+		auth.setShopUrl(shopName);
 		auth.setUserName("dongtze");
 
 		GetRequestExternalModel model = new GetRequestExternalModel();
-		model.setItemUrl(new String[] { "salesjpj083" });
+		model.setItemUrl(new String[] { "nzctx659-10" });
 		GetResponseExternalModel result = null;
 		try {
 			result = port.getInventoryExternal(auth, model);
+			System.out.println(result.getErrCode());
 			if ("N00-000".equals(result.getErrCode())) {
 				GetResponseExternalItem item = result
 						.getGetResponseExternalItem()[0];
+				System.out.println(item);
 			}
 		} catch (RemoteException e) {
 			// TODO: handle exception
+			e.printStackTrace();
 		}
 
 	}
--- src/com/rakuten/util/JdbcConnection.java
+++ src/com/rakuten/util/JdbcConnection.java
@@ -11,10 +11,11 @@ public class JdbcConnection {
 
 //		 String url = "jdbc:mysql://dongtze123.xicp.net:45401/rakuten?user=root&password=admin";
 //		String url = "jdbc:mysql://47.96.28.113:45401/rakuten?user=root&password=admin";
-		String url = "jdbc:mysql://localhost:3306/rakuten?user=root&password=admin";
-//		String url = "jdbc:mysql://192.168.1.5:3306/rakuten?user=root&password=tanluzhe1218";
+//		String url = "jdbc:mysql://localhost:3306/rakuten?user=root&password=admin";
+		String url = "jdbc:mysql://192.168.1.5:3306/rakuten?user=root&password=tanluzhe1218";
 //		String url = "jdbc:mysql://localhost:3306/rakuten?user=root&password=tanluzhe1218";
 //		String url = "jdbc:mysql://192.168.1.106:3306/rakuten?user=root&password=admin";
+//		String url = "jdbc:mysql://60.114.126.175:12586/rakuten?user=root&password=admin";
 		Connection con = DriverManager.getConnection(url);
 		con.setAutoCommit(false);
 		return con;
