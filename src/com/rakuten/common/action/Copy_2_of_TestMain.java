1e36807842c97c7afe186d5a6c32c3608efe8f59
licao <licao@aliyun.com>
2020-09-10 13:52:30 +0800

    Revert "その他プラットホームの発送待ち注文が発送できる対応"
    This reverts commit cfc2c71ebe4b9432b773f248bd1e2ce4d2de928d.

--- src/com/rakuten/common/action/Copy_2_of_TestMain.java
+++ src/com/rakuten/common/action/Copy_2_of_TestMain.java
@@ -4,12 +4,9 @@ import java.rmi.RemoteException;
 
 import javax.xml.rpc.ServiceException;
 
-import org.apache.axis.encoding.Base64;
-
 import jp.co.rakuten.rms.inventoryapi.rms.mall.inventoryapi.Inventoryapi;
 import jp.co.rakuten.rms.inventoryapi.rms.mall.inventoryapi.InventoryapiLocator;
 import jp.co.rakuten.rms.inventoryapi.rms.mall.inventoryapi.InventoryapiPort;
-import shohinmodel.common.Shohincommon;
 import entity.model.v1.inventoryapi.mall.rms.rakuten.co.jp.ExternalUserAuthModel;
 import entity.model.v1.inventoryapi.mall.rms.rakuten.co.jp.GetRequestExternalModel;
 import entity.model.v1.inventoryapi.mall.rms.rakuten.co.jp.GetResponseExternalItem;
@@ -28,29 +25,22 @@ public class Copy_2_of_TestMain {
 			throw e;
 		}
 
-		String shopName = "herz";
-		Shohincommon common = new Shohincommon();
-		String serviceSecret = common.getServiceSecret(shopName);
-		String licenseKey = common.getLicenseKey(shopName);
 		ExternalUserAuthModel auth = new ExternalUserAuthModel();
-		auth.setAuthKey("ESA " + Base64.encode((serviceSecret + ":" + licenseKey).getBytes()));
-		auth.setShopUrl(shopName);
+		auth.setAuthKey("6de01b2c19c810ec4644baa7dc1f30a5");
+		auth.setShopUrl("trend777");
 		auth.setUserName("dongtze");
 
 		GetRequestExternalModel model = new GetRequestExternalModel();
-		model.setItemUrl(new String[] { "nzctx659-10" });
+		model.setItemUrl(new String[] { "salesjpj083" });
 		GetResponseExternalModel result = null;
 		try {
 			result = port.getInventoryExternal(auth, model);
-			System.out.println(result.getErrCode());
 			if ("N00-000".equals(result.getErrCode())) {
 				GetResponseExternalItem item = result
 						.getGetResponseExternalItem()[0];
-				System.out.println(item);
 			}
 		} catch (RemoteException e) {
 			// TODO: handle exception
-			e.printStackTrace();
 		}
 
 	}
